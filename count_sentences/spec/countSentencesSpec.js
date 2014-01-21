32d8dcd8798df241897d6b2f693b2e4b766aeef3
Sara Gorecki <sgorecki@gmail.com>
2014-01-20 23:35:52 -0500

    revise spec so that not all tests are one-word sentences

--- count_sentences/spec/countSentencesSpec.js
+++ count_sentences/spec/countSentencesSpec.js
@@ -1,6 +1,6 @@
 describe( "#is_between", function() {
   it("should return the number of sentences in a string", function() {
-    expect(countSentences("one. two. three?")).toBe(3);
+    expect(countSentences("one. two. or three?")).toBe(3);
   });      
 
   it("should return zero if there are no sentences in a string", function() {
